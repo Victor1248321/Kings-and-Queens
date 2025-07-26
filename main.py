11cad38b10f367c430eb24c08ca7f3203217897a
Master-Magician <Marvin1248@hotmail.com>
2025-07-26 09:39:38 -0500

    Chessboard Created on Screen

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 import pygame
+from screen_data import *
+
+pygame.init()
 
 
+def draw_board():
+    #Size of Each Square of the Board is Calculated
+    square_size = WIDTH // 8 
+
+    for row in range(8):
+        for col in range(8):
+            
+            #Square Color is Determined
+            if (row + col) % 2 == 0:
+                color = WHITE
+            else:
+                color = BLACK
+            
+            
+            pygame.draw.rect(SCREEN, color, (col * square_size, row * square_size, square_size, square_size))
 
 def main():
+    #Welcome Message
     print("Welcome to Kings and Queens!")
+
+    #Game Loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Board is Drawn
+        draw_board()  
+        #The Display is Updated
+        pygame.display.flip() 
+
+    pygame.quit()
+
 
 
 if __name__ == "__main__":
---
+++ screen_data.py
@@ -0,0 +1,11 @@
+import pygame
+
+#This File Contains Values Needed for our Chessboard
+
+WIDTH, HEIGHT = 800, 800
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Chessboard")
+WHITE = (255, 255, 255)
+BLACK = (100, 100, 100)
+
+
